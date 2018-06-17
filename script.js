58acb06344d3301d483b9fa8beed134786e1771e
anascarvalho <39952504+anascarvalho@users.noreply.github.com>
2018-06-17 18:30:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let meninos = ["Andre", "Bruno", "Carlos", "Diego", "Edgar"];
+let meninas = ["Ana", "Brenda", "Cassia", "Duda", "Estela"];
+
+for(i=0; i<5; i++){
+    for(j=0; j<5; j++){
+       console.log(meninos[i] + " X " + meninas[j]);
+    }
+}
